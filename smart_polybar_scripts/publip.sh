5d7c62f383f7c1a2e341279ca9c88ea76c0d4b68
harish2309 <36474847+harish2309@users.noreply.github.com>
2020-05-24 13:37:08 +0530

    Add files via upload

---
+++ smart_polybar_scripts/publip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+int=$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)
+
+if [ $int ]
+then
+
+	IP=$(wget -qO - icanhazip.com)
+	#CUNT=$(curl -s https://ipvigilante.com | jq '.data.country_name' | sed 's/\"//g')
+	CUNT=$(curl -s https://freegeoip.app/xml/ | grep -i City | cut -c 8-30 | cut -d \< -f 1)	
+	echo " $IP, $CUNT"
+else
+	echo "disconnected"
+fi
